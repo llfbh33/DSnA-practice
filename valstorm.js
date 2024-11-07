8583cfe35aa4c3abbf789c1300dab53d2e3ddcc5
aubrie woodbine <aubriewoodbine@yahoo.com>
2024-11-07 16:50:18 -0600

    adjusted aggregate fundtion

--- valstorm.js
+++ valstorm.js
@@ -1,6 +1,5 @@
-import { group } from "d3-array";
-
-export const grouppChartDara = (data, settings) => {
+export const groupChartData = (data, settings, schema) => {
+    console.log('data', data);
     const groupByKey = settings.y[0].key;
 
     const result= [];
@@ -8,9 +7,11 @@ export const grouppChartDara = (data, settings) => {
     const groupedData = {}; // will have a key of the groupByKey value
     const countData = {}; // unknown yet
 
+
     data.forEach(record => {    // a record will hold all fields, we need to isolate the groupByKey field
         const groupValue = record[groupByKey];  // groupValue will be a specific date in this instance
         const groupObj = {};                   // we will store the key of the xField and the value of the count for each of the records which it matches with
+
         settings.x.forEach(xfield => {          // need to loop through each of the x settings for each record to look at all fields
             const xKey = xfield.key;            // xKey will be string key for the specific field we are looking at (not the value of the record)
 
@@ -23,7 +24,14 @@ export const grouppChartDara = (data, settings) => {
             if (settings.aggregate === "Count") {
                 groupObj[xKey] = groupObj[xKey] ? groupObj[xKey] + 1 : 1;
             }
-        })
+            if (settings.aggregate === "Sum" || settings.aggregate === "Average") {
+                groupObj[xKey] = groupObj[xKey] ? groupObj[xKey] + record[xKey] : record[xKey];
+                if (settings.aggregate === "Average") {
+                    countData[groupValue] ? countData[groupValue] += 1 : countData[groupValue] = 1;
+                }
+            }
+        });
+
         if (!groupedData[groupValue]) {
             groupedData[groupValue] = groupObj;
         } else {
@@ -37,10 +45,20 @@ export const grouppChartDara = (data, settings) => {
         const groupObj = groupedData[groupValue];       // object which holds name with val and phone number with val
         const resultObj = { [groupByKey]: groupValue };
 
-        Object.keys(groupObj).forEach(key => {
-            resultObj[key] = groupObj[key];
-        })
-        result.push(resultObj);
-    })
+
+        if (settings.aggregate === "Average") {
+            Object.keys(groupObj).forEach(key => {
+                resultObj[key] = groupObj[key] / countData[groupValue].toFixed(2);
+            })
+            console.log('resultObj', resultObj);
+            result.push(resultObj);
+        } else {
+            Object.keys(groupObj).forEach(key => {
+                resultObj[key] = groupObj[key];
+            })
+            result.push(resultObj);
+        }
+    });
+    console.log('result', result);
     return result;
 }
